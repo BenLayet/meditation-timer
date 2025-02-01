f4db69812e25d962c5b554b6eab97e8a2420ffd6
BenLayet <layetbenjamin@gmail.com>
2025-02-01 23:22:09 +0100

    0: fixes config

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../node_modules/frontend/dist')));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
