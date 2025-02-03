143e92123fdb00e477d0e9032859fa60d9de502e
BenLayet <layetbenjamin@gmail.com>
2025-02-03 09:40:58 +0100

    1: prevent screen to go to sleep

--- packages/frontend/src/Timer.jsx
+++ packages/frontend/src/Timer.jsx
@@ -1,17 +1,25 @@
-import "./Timer.css"
-import {useState, useEffect} from 'react';
-import {onSecondElapsed} from "domain/src/timer/onSecondElapsed.js";
-import {onTimerStarted} from "domain/src/timer/onTimerStarted.js";
+import "./Timer.css";
+import { useState, useEffect, useRef } from 'react';
+import NoSleep from 'nosleep.js';
+import { onSecondElapsed } from "domain/src/timer/onSecondElapsed.js";
+import { onTimerStarted } from "domain/src/timer/onTimerStarted.js";
 
-function Timer({initialState}) {
+function Timer({ initialState }) {
     const [state, setState] = useState(initialState);
+    const noSleep = useRef(new NoSleep());
+
     useEffect(() => {
         const interval = setInterval(() => {
             setState(onSecondElapsed(state));
         }, 1000);
         return () => clearInterval(interval);
-    });
-    const startTimer = () => setState(onTimerStarted(state));
+    }, [state]);
+
+    const startTimer = () => {
+        setState(onTimerStarted(state));
+        noSleep.current.enable();
+    };
+
     const formattedTime = () => formatTime(state.seconds);
 
     return <>
@@ -30,4 +38,5 @@ const formatTime = (totalSeconds) => {
     const secondsStr = String(totalSeconds % 60).padStart(2, '0');
     return hours > 0 ? `${hoursStr}:${minutesStr}:${secondsStr}` : `${minutesStr}:${secondsStr}`;
 };
+
 export default Timer;
