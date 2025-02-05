608e4cc212f52aa1938f6de90de0db7572aa8b75
BenLayet <layetbenjamin@gmail.com>
2025-02-05 15:38:19 +0100

    0: stops gongs if still playing

--- packages/frontend/src/GongService.js
+++ packages/frontend/src/GongService.js
@@ -6,29 +6,11 @@ class GongService {
     play() {
         this.stop();
         this.audio.currentTime = 0;
-        this.audio.volume = 1;
-        this.audio.play();
-    }
-
-    fadeOut() {
-        const fadeOutInterval = setInterval(() => {
-            if (this.audio.volume > 0.2) {
-                this.audio.volume -= 0.2;
-            } else {
-                clearInterval(fadeOutInterval);
-            }
-        }, 100);
+        this.audio.play().then( );
     }
 
     stop() {
         this.audio.pause();
-    }
-    getDuration() {
-        return this.audio.duration;
-    }
-
-    isPlaying(){
-        return !this.audio.paused;
     }
 }
 
--- packages/frontend/src/Timer.jsx
+++ packages/frontend/src/Timer.jsx
@@ -25,6 +25,9 @@ function Timer({totalSeconds}) {
         };
     }, [isTimerRunning(timerState)]);
 
+
+    useEffect(() => () => gongService.stop(), []);
+
     const startTimer = () =>  {
         wakeLockService.requestWakeLock();
         gongService.play();
