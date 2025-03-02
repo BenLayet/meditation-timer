80466d21fbaf0a57ce48c3bc0ce23fbf44fe3189
BenLayet <layetbenjamin@gmail.com>
2025-03-02 10:28:57 +0100

    0: adds animation to statistics

--- packages/frontend/src/components/statistics/Statistics.jsx
+++ packages/frontend/src/components/statistics/Statistics.jsx
@@ -2,25 +2,27 @@ import {useContext} from "react";
 import {AppStateContext} from "../app/AppStateProvider.jsx";
 import {useTranslation} from "react-i18next";
 import {appSelectors} from "domain/src/meditation-timer.app.js";
+import "./Statistics.css";
 
 export function Statistics() {
     const {t} = useTranslation();
     const {state} = useContext(AppStateContext);
     //selectors
     const dailyStreak = appSelectors.statistics.dailyStreak(state);
-    const hourCountThisWeek = appSelectors.statistics.hourCountThisWeek(state);
-    const minuteCountThisWeek = appSelectors.statistics.minuteCountThisWeek(state);
+    const hourCount = appSelectors.statistics.hourCountThisWeek(state);
+    const minuteCount = appSelectors.statistics.minuteCountThisWeek(state);
     const shouldTotalMinutesThisWeekBeDisplayed = appSelectors.statistics.shouldTotalMinutesThisWeekBeDisplayed(state);
     const shouldDailyStreakBeDisplayed = appSelectors.statistics.shouldDailyStreakBeDisplayed(state);
 
     return (
-        <div>
-            {shouldDailyStreakBeDisplayed && <p>{t("dailyStreak", {dailyStreak})}</p>}
-            {shouldTotalMinutesThisWeekBeDisplayed && <p>
-                {t("hourCount", hourCountThisWeek)}
-                {t("minuteCount", minuteCountThisWeek)}
-                {t("thisWeek")}
-            </p>}
-        </div>
+        <ul>
+            {shouldTotalMinutesThisWeekBeDisplayed &&
+                <li>{t("hourCount", hourCount)}
+                    {t("minuteCount", minuteCount)}
+                    {t("thisWeek")}</li>}
+            {shouldDailyStreakBeDisplayed &&
+                <li>{t("dailyStreak", {dailyStreak})}</li>}
+
+        </ul>
     )
 }
