c834bbc1a3752dd2f6134418dadca345e8e8a9bd
BenLayet <layetbenjamin@gmail.com>
2025-03-09 16:18:26 +0100

    12: fixes chained-events.spec.js

--- packages/domain/src/lib/chained-events.spec.js
+++ packages/domain/src/lib/chained-events.spec.js
@@ -1,10 +1,10 @@
 import {describe, expect, test} from 'vitest';
-import {createEventFactory} from "./event-factory.js";
 import {createChainedEventFactories} from "./chained-events.js";
+import ow from "ow";
 
-const eventOneOccurred = createEventFactory('eventOneOccurred', ({key1}) => ({key1}));
-const eventTwoOccurred = createEventFactory('eventTwoOccurred', ({key1}) => ({key1}));
-const eventThreeOccurred = createEventFactory('eventThreeOccurred', ({key1, key2}) => ({key1, key2}));
+const eventOneOccurred = {key1: ow.string};
+const eventTwoOccurred = {key1: ow.string};
+const eventThreeOccurred = {key1: ow.string, key2: ow.string};
 const simpleEventChain = {
     onEvent: eventOneOccurred,
     thenDispatch: eventTwoOccurred
@@ -19,7 +19,7 @@ describe('createAllComponentEvents', () => {
     test('simple event chain: when event one occurs, event two with same payload should be created', () => {
         //given
         const component = {chainedEvents: [simpleEventChain]};
-        const previousEvent = eventOneOccurred({key1: 'value1'});
+        const previousEvent = {eventType: eventOneOccurred, payload: {key1: 'value1'}};
         const state = {};
         const chainedEventsFactory = createChainedEventFactories(component);
 
@@ -27,35 +27,41 @@ describe('createAllComponentEvents', () => {
         const chainedEvents = chainedEventsFactory(previousEvent, state);
 
         //then
-        expect(chainedEvents).toEqual([eventTwoOccurred({key1: 'value1'})]);
+        expect(chainedEvents).toEqual([{eventType: eventTwoOccurred, payload: {key1: 'value1'}}]);
     });
     test('event chain with payload: when event one occurs, event three with constructed payload should be created', () => {
         //given
         const component = {chainedEvents: [eventChainWithPayload]}
-        const previousEvent = eventOneOccurred({key1: 'value1'});
+        const previousEvent = {eventType: eventOneOccurred, payload: {key1: 'value1'}};
         const state = {property1: 'stateProperty1'};
         const eventChain = createChainedEventFactories(component);
 
         //when
-        const propagated = eventChain(previousEvent, state);
+        const newEvents = eventChain(previousEvent, state);
 
         //then
-        expect(propagated).toEqual([eventThreeOccurred({key1: 'value1', key2: 'constructed', key3: 'stateProperty1'})]);
+        expect(newEvents).toEqual([{
+            eventType: eventThreeOccurred,
+            payload: {key1: 'value1', key2: 'constructed', key3: 'stateProperty1'}
+        }]);
     });
     test('sub-component with chain with payload: when event one occurs, sub-components should dispatch event three with constructed payload based on sub state', () => {
         //given
         const subComponent = {chainedEvents: [eventChainWithPayload]};
         const component = {subComponents: {subComponent1: subComponent}};
 
-        const previousEvent = eventOneOccurred({key1: 'value1'});
+        const previousEvent = {eventType: eventOneOccurred, payload: {key1: 'value1'}};
         const state = {subComponent1: {property1: 'stateProperty1'}};
         const eventChain = createChainedEventFactories(component);
 
         //when
-        const propagated = eventChain(previousEvent, state);
+        const newEvents = eventChain(previousEvent, state);
 
         //then
-        expect(propagated).toEqual([eventThreeOccurred({key1: 'value1', key2: 'constructed', key3: 'stateProperty1'})]);
+        expect(newEvents).toEqual([{
+            eventType: eventThreeOccurred,
+            payload: {key1: 'value1', key2: 'constructed', key3: 'stateProperty1'}
+        }]);
 
     });
 
@@ -64,8 +70,7 @@ describe('createAllComponentEvents', () => {
         const subComponent2 = {chainedEvents: [eventChainWithPayload]};
         const subComponent1 = {subComponents: {subComponent2}};
         const component = {subComponents: {subComponent1}};
-
-        const previousEvent = eventOneOccurred({key1: 'value1'});
+        const previousEvent = {eventType: eventOneOccurred, payload: {key1: 'value1'}};
         const state = {subComponent1: {subComponent2: {property1: 'stateProperty1'}}};
         const eventChain = createChainedEventFactories(component);
 
@@ -73,7 +78,10 @@ describe('createAllComponentEvents', () => {
         const newEvents = eventChain(previousEvent, state);
 
         //then
-        expect(newEvents).toEqual([eventThreeOccurred({key1: 'value1', key2: 'constructed', key3: 'stateProperty1'})]);
+        expect(newEvents).toEqual([{
+            eventType: eventThreeOccurred,
+            payload: {key1: 'value1', key2: 'constructed', key3: 'stateProperty1'}
+        }]);
 
     })
 });
