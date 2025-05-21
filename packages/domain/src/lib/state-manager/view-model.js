7010ae3854d48f9fb7cd1fb8abea905c7f0bfb99
BenLayet <layetbenjamin@gmail.com>
2025-05-21 19:40:57 +0200

    14: fixes domain unit tests

--- packages/domain/src/lib/state-manager/view-model.js
+++ packages/domain/src/lib/state-manager/view-model.js
@@ -1,25 +1,38 @@
-import {map} from "../functions/object.functions.js";
-import {createEvent} from "./create-event.js";
+import { map } from "../functions/object.functions.js";
+import { createEvent } from "./create-event.js";
 
 const selectors = (component, state) =>
-    map(component.selectors ?? {}, selector => () => selector(state));
+  map(component.selectors ?? {}, (selector) => () => selector(state));
 
 const dispatchers = (component, dispatch, componentPath) =>
-    map(component.events ?? {}, ({payloadShape, eventType}) => (payload = {}, isNewCycle=true) => {
-        const event = createEvent({payloadShape, eventType}, componentPath, payload, isNewCycle);
+  map(
+    component.events ?? {},
+    ({ payloadShape, eventType }) =>
+      (payload = {}, isNewCycle = false) => {
+        const event = createEvent(
+          { payloadShape, eventType },
+          componentPath,
+          payload,
+          isNewCycle,
+        );
         dispatch(event);
-    });
+      },
+  );
 
 const children = (component, state, dispatch, componentPath) =>
-    map(component.children ?? {}, (childComponent, childName) =>
-        getVM(
-            childComponent,
-            (state.children && state.children[childName]) ? state.children[childName] : {},
-            dispatch,
-            [...componentPath, childName]));
+  map(component.children ?? {}, (childComponent, childName) =>
+    getVM(
+      childComponent,
+      state.children && state.children[childName]
+        ? state.children[childName]
+        : {},
+      dispatch,
+      [...componentPath, childName],
+    ),
+  );
 
 export const getVM = (component, state, dispatch, componentPath = []) => ({
-    selectors: selectors(component, state),
-    dispatchers: dispatchers(component, dispatch, componentPath),
-    children: children(component, state, dispatch, componentPath),
-})
+  selectors: selectors(component, state),
+  dispatchers: dispatchers(component, dispatch, componentPath),
+  children: children(component, state, dispatch, componentPath),
+});
