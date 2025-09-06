95c2ec551ba4a1c0063050363833ecb58aa806c0
BenLayet <layetbenjamin@gmail.com>
2025-09-06 11:44:36 +0200

    0: fixes preparation layout

--- modules/frontend/src/components/preparation/Preparation.jsx
+++ modules/frontend/src/components/preparation/Preparation.jsx
@@ -9,7 +9,7 @@ function Preparation({ vm }) {
   const preparationRemainingTime = vm.selectors.remainingTime();
   const timeIncrementInSeconds = vm.selectors.timeIncrementInSeconds();
   return (
-    <>
+    <div className="vstack">
       <label>{t("preparation")}</label>
       <div className="mt-time-display">{preparationRemainingTime}</div>
       <div className="d-flex flex-column fs-4">
@@ -23,7 +23,7 @@ function Preparation({ vm }) {
           {t("skip")}
         </a>
       </div>
-    </>
+    </div>
   );
 }
 
